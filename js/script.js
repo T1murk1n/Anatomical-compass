70d1e736130bb2ca5ee50851803928c7b4a3ad39
Timur <tgizzyatov@mail.ru>
2023-12-24 15:00:06 +0400

    Burger menu

--- js/script.js
+++ js/script.js
@@ -13,8 +13,16 @@ if(btnDownload) {
 // Меню
 let menu = document.querySelector(".menu");
 let btnMenu = document.querySelector(".menu-button");
+let mainZone = document.querySelector("main.main");
 if(menu && btnMenu) {
     btnMenu.addEventListener('click', () => {
         menu.classList.toggle('active');
-    })
+        btnMenu.classList.toggle('animate');
+    });
+    mainZone.addEventListener('click', () => {
+        if(menu.classList.contains('active')) {
+            menu.classList.remove('active');
+            btnMenu.classList.remove('animate');
+        }
+    });
 }
